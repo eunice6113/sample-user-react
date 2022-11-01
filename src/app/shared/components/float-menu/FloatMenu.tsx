963ddad8687ffd7312564ff45dcab2ffc1c79c6b
Eunice <eunice6113@gmail.com>
2022-11-01 16:07:45 +0900

    css 수정

--- src/app/shared/components/float-menu/FloatMenu.tsx
+++ src/app/shared/components/float-menu/FloatMenu.tsx
@@ -6,26 +6,33 @@ import './float-menu.css'
 const FloatMenu = () => {
     
     return (
-        <ul className="floatMenuBox">
-            <li>
-                <Link to='/asc/cat-req'>
-                    <i className="pi pi-shopping-cart icon-cart"></i>
-                    <p>장바구니</p>
-                </Link>
-            </li>
-            <li>
-                <button className='faqBtn'>
-                    <i className="icon-req"></i>
-                    <p>건의 및 개선</p>
-                </button>
-            </li>
-            <li>
-                <Link to='/spr/faq'>
-                    <i className="icon-faq"></i>
-                    <p>자주묻는질문</p>
-                </Link>
-            </li>
-        </ul>
+        <>
+            <ul className="floatMenuBox">
+                <li>
+                    <Link to='/asc/cat-req'>
+                        <i className="pi pi-shopping-cart icon-cart"></i>
+                        <p>장바구니</p>
+                    </Link>
+                </li>
+                <li>
+                    <button className='faqBtn'>
+                        <i className="icon-qna"></i>
+                        <p>건의 및 개선</p>
+                    </button>
+                </li>
+                <li>
+                    <Link to='/spr/faq'>
+                        <i className="icon-faq"></i>
+                        <p>자주묻는질문</p>
+                    </Link>
+                </li>
+            </ul>
+
+            {/* 건의 및 개선 */}
+            <div className="qnaBox">
+
+            </div>
+        </>
     )
 }
 export default FloatMenu;
